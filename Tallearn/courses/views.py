2b0440520db949948b000ec26c6204f75a426a8f
Rezurgat <Killa.S.V.ru@bk.ru>
2022-04-28 22:11:22 +0300

    Started work with courses page

--- Tallearn/courses/views.py
+++ Tallearn/courses/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
+from courses.models import Category
+
+
+class CategoryListView(ListView):
+    model = Category
+
+    def get_queryset(self):
+        return Category.objects.all()
 
 def home(request):
-    return render(request, 'base.html')
+    return render(request, 'home.html')
