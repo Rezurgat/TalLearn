d1026adbf288815f24818ae0e9fc16df27e3f746
Rezurgat <Killa.S.V.ru@bk.ru>
2022-05-07 13:07:13 +0300

    Refactored forms.py(included widjets).Refactored sass for form fields

--- Tallearn/courses/forms.py
+++ Tallearn/courses/forms.py
@@ -6,3 +6,9 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         exclude = ['create_at', 'course']
+        widgets = {
+         'firstname': forms.TextInput(attrs={'placeholder': 'Your Firstname'}),
+         'lastname': forms.TextInput(attrs={'placeholder': 'Your Lastname'}),
+         'email': forms.Textarea(attrs={'cols': 87, 'rows': 1, 'placeholder': 'Your Email'}),
+         'comment': forms.Textarea(attrs={'cols': 87, 'rows': 10, 'placeholder': 'Your Message'}),
+        }
