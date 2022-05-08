ceb3c154dde5eb44daa3a097e57afc62fbc23c4c
Rezurgat <Killa.S.V.ru@bk.ru>
2022-05-08 23:43:38 +0300

    Added contact model; created view for contact model

--- Tallearn/about/urls.py
+++ Tallearn/about/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-   path('about/', views.AboutListView.as_view(), name='about_list')
+   path('about/', views.AboutListView.as_view(), name='about_list'),
+   path('contact/', views.ContactListView.as_view(), name='contact_list'),
+
 
 ]
