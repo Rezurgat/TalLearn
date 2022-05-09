a82311a3bab27507f97582d1bb2951a3c267f50b
Rezurgat <Killa.S.V.ru@bk.ru>
2022-05-09 15:04:09 +0300

    Finished work with feedback form.Added message.success

--- Tallearn/about/urls.py
+++ Tallearn/about/urls.py
@@ -5,7 +5,7 @@ from .import views
 
 urlpatterns = [
    path('about/', views.AboutListView.as_view(), name='about_list'),
-   path('contact/', views.ContactListView.as_view(), name='contact_list'),
-
+   path('contact/', views.ContactView.as_view(), name='contact'),
+   path('feedback/', views.CreateFeedback.as_view(), name='feedback')
 
 ]
