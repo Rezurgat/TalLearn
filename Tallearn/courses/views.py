e02000e6d1530b93af7709e8ef25c97749649145
Rezurgat <Killa.S.V.ru@bk.ru>
2022-05-14 20:04:02 +0300

    Finished work with dropdown menu (top-menu)

--- Tallearn/courses/views.py
+++ Tallearn/courses/views.py
@@ -31,6 +31,7 @@ class CourseDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['form'] = CommentForm()
+        context['course_list'] = Course.objects.all()
         return context
 
 
