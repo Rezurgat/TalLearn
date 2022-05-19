633e9780be609b3a423f9c2b24a9133efba6e8d1
Rezurgat <Killa.S.V.ru@bk.ru>
2022-05-19 20:52:31 +0300

    Fixed:posts showing in contact_list.html (refactored views)

--- Tallearn/about/views.py
+++ Tallearn/about/views.py
@@ -1,10 +1,11 @@
 from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render
-from django.views import View
 from django.views.generic import ListView, CreateView
 
 from about.models import About, Contact, Feedback
 from about.forms import FeedbackForm
+
+from blog_n_events.models import Post
 
 
 class AboutListView(ListView):
@@ -13,17 +14,27 @@ class AboutListView(ListView):
     def get_queryset(self):
         return About.objects.all()
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['blog'] = Post.objects.order_by('-create_at')[0:3]
+        context['contact'] = Contact.objects.all()
+        return context
 
-class ContactView(View):
+
+class ContactListView(ListView):
+    model = Contact
+
     def get(self, request):
         contacts = Contact.objects.all()
+        blog = Post.objects.order_by('-create_at')[0:3]
         form = FeedbackForm()
-        return render(request, 'about/contact.html', {"contacts": contacts, "form": form})
+        return render(request, 'about/contact_list.html', {"contacts": contacts, "form": form, 'blog': blog})
+
 
 
 class CreateFeedback(SuccessMessageMixin, CreateView):
     form_class = FeedbackForm
-    template_name = 'about/contact.html'
+    template_name = 'about/contact_list.html'
     success_message = "Message was created successfully"
 
     def get_success_url(self):
