a435388888a021bcba030d112c1e9a899d88a8ec
Rezurgat <Killa.S.V.ru@bk.ru>
2022-08-10 20:53:17 +0300

    Added filtration for blog post

--- Tallearn/api/urls.py
+++ Tallearn/api/urls.py
@@ -3,8 +3,8 @@ from api.blog_n_events_api.views_bne_api import BlogPostApiView, CategoryPostApi
 from api.about_api.views_about_api import AboutApiView, ContactApiView
 
 urlpatterns = [
-    path('api/v1/blogpostlist', BlogPostApiView.as_view()),
     path('api/v1/categorypostlist', CategoryPostApiView.as_view()),
+    path('api/v1/postlist/<int:category>', BlogPostApiView.as_view()),
     path('api/v1/eventlist', EventApiView.as_view()),
 
     path('api/v1/aboutlist', AboutApiView.as_view()),
