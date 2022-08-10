e9b6e7e59ac6cc9298bfa0cfc7a60177b6fd7559
Rezurgat <Killa.S.V.ru@bk.ru>
2022-08-10 19:16:48 +0300

    Started work with about_api

---
+++ Tallearn/api/about_api/serializer_about_api.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from about.models import About, Contact, Feedback
+
+class AboutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = About
+        fields = ('firstname', 'lastname', 'proficiency_level', 'bio', 'link_telegram', 'link_instagram', 'link_vk', 'create_at')
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        fields = ('firstname', 'phone', 'link_telegram', 'link_website')
+
