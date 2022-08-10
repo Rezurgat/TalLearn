c03960eaa8f22e3223f7134756cb055b30b92a60
Rezurgat <Killa.S.V.ru@bk.ru>
2022-08-10 18:46:21 +0300

    Finished work with blog_n_events serializers

--- Tallearn/api/blog_n_events_api/views_bne_api.py
+++ Tallearn/api/blog_n_events_api/views_bne_api.py
@@ -1,9 +1,17 @@
 from rest_framework import generics
-from blog_n_events.models import Post
-from api.blog_n_events_api.serializer_bne_api import BlogPostSerializer
+from blog_n_events.models import Post, Category, Event
+from api.blog_n_events_api.serializer_bne_api import BlogPostSerializer, СategoryPostSerializer, EventSerializer
+
+class CategoryPostApiView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = СategoryPostSerializer
 
 class BlogPostApiView(generics.ListAPIView):
     queryset = Post.objects.all()
     serializer_class = BlogPostSerializer
+
+class EventApiView(generics.ListAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
 
 
